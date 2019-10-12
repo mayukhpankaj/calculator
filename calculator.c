09ef0668a71f8f142569ab765c589e1754eea9e4
mayukhpankaj <40158577+mayukhpankaj@users.noreply.github.com>
2019-10-12 04:27:55 -0700

    ver1

---
+++ calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+
+  int a,b,c;
+   
+  printf("enter two numbers");
+  
+  scanf("%d %d", &a,&b);
+  
+  
+  if(c=1)
+  printf("%d",a+b);
+  
+  
+  return 0;
+  
+  }
